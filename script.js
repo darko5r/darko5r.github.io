80ca6ef62e6d81ea04d38cf2189c9d5a4bf7fcca
darko5r <darkop@riseup.net>
2023-02-05 17:53:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 
 var apiKey = process.env.API_KEY;
 var authDomain = process.env.AUTHDOMAIN;
+var databaseURL = firebaseConfig.databaseURL;
+var projectId = firebaseConfig.projectId;
+var storageBucket = firebaseConfig.storageBucket;
+var messagingSenderId = firebaseConfig.messagingSenderId;
+var appId = firebaseConfig.appId;
+var measurementId = firebaseConfig.measurementId;
 
