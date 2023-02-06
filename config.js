b733e6a5f43e90b88177a69d32d7c7f744c7c5d1
darko5r <darkop@riseup.net>
2023-02-06 02:14:50 +0100

    update ignored files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
+        apiKey: "AIzaSyBB00grLIfvhq7EEQV6qiZUZuayP4-GoAg",
         authDomain: "blog-25253.firebaseapp.com",
         databaseURL: "https://blog-25253-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "blog-25253",
